e93d872865727a8deab8c4026e66a9d549e38e06
KarimAziev <karim.aziiev@gmail.com>
2026-06-01 15:48:34 +0300

    fix: support TOML parsing on Python <3.11

--- build123d_ocp_preview/config.py
+++ build123d_ocp_preview/config.py
@@ -1,8 +1,12 @@
 import sys
-import tomllib
 from collections.abc import Sequence
 from dataclasses import dataclass
 from pathlib import Path
+
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
 
 
 @dataclass(frozen=True)
